b12b9f08d2a1e11f7736cc3d67bec88577513029
juanmont <juanmont@ucm.es>
2016-12-16 13:16:11 +0100

    inserccion de datos

--- gr07_mongoengine.py
+++ gr07_mongoengine.py
@@ -15,7 +15,7 @@ ni perjudicar los resultados de los demás.
 
 """
 
-from mongoengine import connect
+from mongoengine import *
 #import barcode
 
 
@@ -23,68 +23,21 @@ from mongoengine import connect
 connect('giw_mongoengine')
 
 #Comenzamos a crear las clases que heredarán de Document ya que son esquemas fijos
-class dni(EmbeddedDocument):
-    numero= intField(required=True, max_length=8, min_length=8) 
+class Dni(EmbeddedDocument):
+    numero= IntField(required=True, max_length=8, min_length=8) 
     letra= StringField(required=True, min_length=1, max_length=1)
 
     def clean(self):
-        listLetter ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
+        listLetter = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
         if(self.letra != listLetter[self.numero%23]):
             raise ValidationError("DNI no valido")
 
-
-class Usuario(Document):
-# DNI (obligatorio, ´unico)
-# Nombre (obligatorio)
-# Primer apellidos (obligatorio)
-# Segundo apellido (opcional)
-# Fecha de nacimiento (obligatorio, formato ’AAAA-MM-DD’)
-# Fecha de los ´ultimos 10 accesos al sistema (opcional, las fechas tienen el
-# formato ’AAAA,MM,DD,HH,MM,SS,NNNNNN’)
-# Lista de tarjetas de cr´edito (opcional)
-# Lista de referencias a pedidos (opcional)
-
-    dni = EmmbeddedDocumentField(dni, required=True, unique=True)
-    nombre = StringField(required = True)
-    apellido_1 = StringField(required = True)
-    apellido_2 = StringField()
-    fecha_nac = DateTimeField(required = True)
-    fecha_accesos = ComplexDateTimeField(max_length = 10)
-    tarjetas = ListField(ReferenceField(Tarjeta, reverse_delete_rule = CASCADE))
-    pedidos = ListField(ReferenceField(Pedido, reverse_delete_rule=CASCADE))
-
-    
 class Tarjeta(Document):
     nombre_propietario = StringField(required=True)
-    numero = intField(primary_key=True, required = True, min_length=16, max_length=16)
-    caducidad_mes = intField(required = True, min_length=1, max_length=2, min_value=1, max_value=12)
-    caducidad_ano = intField(required=True, min_length=4 ,max_length=4, min_value=1800)
-    cvv = intField(required=True, min_length=3, max_length=3)
-
-class Pedido(Document):
-    total = FloatField(required=True, min_value=0.0)
-    fecha = DateTimeField(required=True)
-    linea_pedido = ListField(Linea_Pedido, required=True)
-
-    def clean(self):
-        total = 0
-        for l in self.linea_pedido:
-            total += l.total
-        if (self.total != total):
-            raise ValidationError("El total del pedido no coincide")
-
-class Linea_Pedido(Document):
-    cantidad_productos = intField(required=True, min_value = 1)
-    precio_producto = FloatField(required=True, min_value=0.0)
-    nombre_producto = StringField(required=True)
-    total = FloatField(required=True, min_value=0)
-    referencia_producto = ReferenceField(Producto, required=True)
-
-    def clean(self):
-        if ((self.cantidad_productos * self.precio_producto) != self.total):
-            raise ValidationError("El total de la linea no coincide")
-        if(self.nombre_producto != self.referencia_producto.nombre):
-            raise ValidationError("Nombre de producto no valido")
+    numero = IntField(primary_key=True, required = True, min_length=16, max_length=16)
+    caducidad_mes = IntField(required = True, min_length=1, max_length=2, min_value=1, max_value=12)
+    caducidad_ano = IntField(required=True, min_length=4 ,max_length=4, min_value=1800)
+    cvv = IntField(required=True, min_length=3, max_length=3)
 
 class Producto(Document):
     codigo = IntField(min_value=0, unique=True)
@@ -105,11 +58,57 @@ class Producto(Document):
         ean = barcode.get('ean13', valor, writer=barcode.writer.ImageWriter())
         # mostramos el codigo de barras en consola
         print ean.to_ascii()
+        
+class Linea_Pedido(Document):
+    cantidad_productos = IntField(required=True, min_value = 1)
+    precio_producto = FloatField(required=True, min_value=0.0)
+    nombre_producto = StringField(required=True)
+    total = FloatField(required=True, min_value=0)
+    referencia_producto = ReferenceField(Producto, required=True)
+
+    def clean(self):
+        if ((self.cantidad_productos * self.precio_producto) != self.total):
+            raise ValidationError("El total de la linea no coincide")
+        if(self.nombre_producto != self.referencia_producto.nombre):
+            raise ValidationError("Nombre de producto no valido")
+            
+class Pedido(Document):
+    total = FloatField(required=True, min_value=0.0)
+    fecha = DateTimeField(required=True)
+    linea_pedido = ListField(Linea_Pedido, required=True)
+
+    def clean(self):
+        total = 0
+        for l in self.linea_pedido:
+            total += l.total
+        if (self.total != total):
+            raise ValidationError("El total del pedido no coincide")
+
+class Usuario(Document):
+# DNI (obligatorio, ´unico)
+# Nombre (obligatorio)
+# Primer apellidos (obligatorio)
+# Segundo apellido (opcional)
+# Fecha de nacimiento (obligatorio, formato ’AAAA-MM-DD’)
+# Fecha de los ´ultimos 10 accesos al sistema (opcional, las fechas tienen el
+# formato ’AAAA,MM,DD,HH,MM,SS,NNNNNN’)
+# Lista de tarjetas de cr´edito (opcional)
+# Lista de referencias a pedidos (opcional)
+
+    dni = EmbeddedDocumentField(Dni, required=True, unique=True)
+    nombre = StringField(required = True)
+    apellido_1 = StringField(required = True)
+    apellido_2 = StringField()
+    fecha_nac = DateTimeField(required = True)
+    fecha_accesos = ComplexDateTimeField(max_length = 10)
+    tarjetas = ListField(ReferenceField(Tarjeta, reverse_delete_rule = CASCADE))
+    pedidos = ListField(ReferenceField(Pedido, reverse_delete_rule=CASCADE))
 
 
 producto = Producto(codigo = 1234567890418, nombre="producto1", categoria=1)
+producto.drop_collection()
 producto.save()
-linea = Linea_Pedido(cantidad_productos=2, precio_producto=2, nombre_producto="producto1", total=5, referencia_producto=producto)
+linea = Linea_Pedido(cantidad_productos=2, precio_producto=2, nombre_producto="producto1", total=4, referencia_producto=producto)
 linea.save()
 
 
